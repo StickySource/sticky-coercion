e98697130f37523cbd8a9d3d5802df9adbfc2f5d
Michael McCallum <michael@StickyCode.net>
2011-03-18 22:59:52 +1300

    A template test that lets you defines * values and there coercion results * the coercion * coercion targets * inapplicable types
    It will test the various collection and array processing as well

---
+++ src/test/java/net/stickycode/coercion/AbstractCoercionTest.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright (c) 2011 RedEngine Ltd, http://www.redengine.co.nz. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package net.stickycode.coercion;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public abstract class AbstractCoercionTest {
+
+  @Rule
+  public TestName testName = new TestName();
+
+  protected abstract Coercion<?> coercion();
+
+  protected abstract String firstValue();
+
+  protected abstract String secondValue();
+
+  protected abstract String thirdValue();
+
+  private String twoValues() {
+    return firstValue() + "," + secondValue();
+  }
+
+  private String threeValues() {
+    return firstValue() + "," + secondValue() + "," + thirdValue();
+  }
+
+
+  protected abstract Object firstResult();
+
+  protected abstract Object secondResult();
+
+  protected abstract Object thirdResult();
+
+  public AbstractCoercionTest() {
+    super();
+  }
+
+  @Test
+  public void isApplicable() {
+    assertThat(coercion().isApplicableTo(coercionType(getApplicableType()))).isTrue();
+    assertThat(coercion().isApplicableTo(coercionType(getInapplicableType()))).isFalse();
+  }
+
+  @Test
+  public void coerce() {
+    assertThat(firstResult()).isEqualTo(firstResult());
+    assertThat(coercion().coerce(coercionType(getApplicableType()), firstValue())).isEqualTo(firstResult());
+    assertThat(coercion().coerce(coercionType(getApplicableType()), secondValue())).isEqualTo(secondResult());
+    assertThat(coercion().coerce(coercionType(getApplicableType()), thirdValue())).isEqualTo(thirdResult());
+  }
+
+  protected abstract Class<?> getInapplicableType();
+
+  protected abstract Class<?> getApplicableType();
+
+  @Test
+  public void list() {
+    assertThat(coerceCollection(firstValue())).containsOnly(firstResult());
+    assertThat(coerceCollection(twoValues())).containsOnly(firstResult(), secondResult());
+    assertThat(coerceCollection(threeValues())).containsOnly(firstResult(), secondResult(), thirdResult());
+  }
+
+  @Test
+  public void set() {
+    assertThat(coerceCollection(firstValue())).containsOnly(firstResult());
+    assertThat(coerceCollection(twoValues())).containsOnly(firstResult(), secondResult());
+    assertThat(coerceCollection(threeValues())).containsOnly(firstResult(), secondResult(), thirdResult());
+  }
+
+  @Test
+  public void queue() {
+    assertThat(coerceCollection(firstValue())).containsOnly(firstResult());
+    assertThat(coerceCollection(twoValues())).containsOnly(firstResult(), secondResult());
+    assertThat(coerceCollection(threeValues())).containsOnly(firstResult(), secondResult(), thirdResult());
+  }
+
+  @Test
+  public void array() {
+    assertThat(coerceArray(firstValue())).containsOnly(firstResult());
+    assertThat(coerceArray(twoValues())).containsOnly(firstResult(), secondResult());
+    assertThat(coerceArray(threeValues())).containsOnly(firstResult(), secondResult(), thirdResult());
+  }
+
+  @Test
+  public void collection() {
+    assertThat(coerceCollection(firstValue())).containsOnly(firstResult());
+    assertThat(coerceCollection(twoValues())).containsOnly(firstResult(), secondResult());
+    assertThat(coerceCollection(threeValues())).containsOnly(firstResult(), secondResult(), thirdResult());
+  }
+
+  protected CoercionTarget coercionType(final Class<?> type) {
+    return new CoercionType(type);
+  }
+
+  private Field getField(String name) {
+    try {
+      return getClass().getDeclaredField(name);
+    }
+    catch (SecurityException e) {
+      throw new RuntimeException(e);
+    }
+    catch (NoSuchFieldException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  protected Collection<Object> coerceCollection(String value) {
+    return (Collection<Object>) collectionCoercion().coerce(componentCoercionType(), value);
+  }
+
+  protected Object[] coerceArray(String value) {
+    return (Object[]) arrayCoercion().coerce(coercionType(getApplicableArrayType()), value);
+  }
+
+  protected abstract Class<?> getApplicableArrayType();
+
+  private CoercionTarget componentCoercionType() {
+    String name = testName.getMethodName();
+    Field field = getField(name);
+    return new CoercionType(field.getType(), (ParameterizedType) field.getGenericType());
+  }
+
+  @SuppressWarnings("unchecked")
+  private CollectionCoercion collectionCoercion() {
+    return new CollectionCoercion(Arrays.asList(coercion()));
+  }
+
+  @SuppressWarnings("unchecked")
+  private ArrayCoercion arrayCoercion() {
+    return new ArrayCoercion(Arrays.asList(coercion()));
+  }
+
+}
