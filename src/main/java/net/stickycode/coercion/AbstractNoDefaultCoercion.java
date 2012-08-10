f60dd3441d088f6ca3bae281ccc098b7fe0b04c1
Michael McCallum <michael@StickyCode.net>
2012-08-10 22:25:05 +1200

    The no default coercion should never be the parent of something with a default... make the methods final to prevent extension

--- src/main/java/net/stickycode/coercion/AbstractNoDefaultCoercion.java
+++ src/main/java/net/stickycode/coercion/AbstractNoDefaultCoercion.java
@@ -4,12 +4,12 @@ public abstract class AbstractNoDefaultCoercion<T>
     implements Coercion<T> {
 
   @Override
-  public boolean hasDefaultValue() {
+  final public boolean hasDefaultValue() {
     return false;
   }
 
   @Override
-  public T getDefaultValue(CoercionTarget target) {
+  final public T getDefaultValue(CoercionTarget target) {
     throw new UnsupportedOperationException("This coercion " + toString() + " does not define a default value");
   }
 
